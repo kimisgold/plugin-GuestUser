d72c4fd489425c6ffcbc1d64b1e873cf45c10e71
Kim Nguyen <design@kimisgold.com>
2014-01-03 12:52:31 -0500

    Only show register message if it has a value.

--- views/public/javascripts/guest-user.js
+++ views/public/javascripts/guest-user.js
@@ -8,15 +8,15 @@ jQuery('document').ready(function() {
         var welcomeTextLeft = jQuery('#menu-admin-bar-welcome').offset().left;
         var guMenu = jQuery('#menu-guest-user-me').closest('ul');
         guMenu.offset({left:welcomeTextLeft, top:31});        
+        jQuery('a#menu-admin-bar-welcome').closest('li').hover(
+                function() {jQuery('#menu-guest-user-me').closest('ul').show();},
+                function() {jQuery('#menu-guest-user-me').closest('ul').hide();}
+            );
+    
+        jQuery('#menu-guest-user-register').closest('li').hover(
+                function() {jQuery('#guest-user-register-info').show();},
+                function() {jQuery('#guest-user-register-info').hide();}
+            );
     }
 
-    jQuery('a#menu-admin-bar-welcome').closest('li').hover(
-            function() {jQuery('#menu-guest-user-me').closest('ul').show();},
-            function() {jQuery('#menu-guest-user-me').closest('ul').hide();}
-        );
-
-    jQuery('#menu-guest-user-register').closest('li').hover(
-            function() {jQuery('#guest-user-register-info').show();},
-            function() {jQuery('#guest-user-register-info').hide();}
-        );
 });
